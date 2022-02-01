f6f44cdb257abde802c2a5176f78c844d2b0820a
Rokas214 <rokas.guzys@gmail.com>
2022-02-01 19:12:48 +0200

    Created a post method

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const mysql = require("mysql2/promise");
 
 const app = express();
 app.use(cors());
@@ -7,6 +8,10 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send({ msg: "Working" });
+});
+
+app.post("/post", (req, res) => {
+	res.send(req.body);
 });
 
 const port = 8080;
